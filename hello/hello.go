23b71a62ad37773ed88c8ed6f6fc0a3bb32dcc7b
almeidabbm <almeidabatistam@gmail.com>
2022-10-28 10:42:38 +0100

    Initial commit

---
+++ hello/hello.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"example.com/greetings"
+	"fmt"
+	"log"
+)
+
+func main() {
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+  names := []string{
+    "Bruno",
+    "Ines",
+    "Someone else",
+  }
+
+	greetings, err := greetings.Hellos(names)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(greetings)
+}
